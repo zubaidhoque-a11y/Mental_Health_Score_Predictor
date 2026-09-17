ecc5ad4da4d7547721ca81e5c34c2a38e7857e98
JUBAID HOQUE <jubaidhoque@JUBAIDs-MacBook-Air.local>
2026-09-18 02:00:01 +0530

    All updated

---
+++ main.py
@@ -0,0 +1,71 @@
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Literal
+from fastapi.middleware.cors import CORSMiddleware
+
+model = joblib.load('Mental_Health_Model.pkl')
+top_countries = ['Other','India','USA','Canada','Australia','UK','Germany','Mexico','Turkey','France']
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+#A first Pydantic Model
+class StudentData(BaseModel):
+    age                     : int = Field(..., ge=10, le=100)
+    gender                  : Literal['Male', 'Female']
+    country                 : str
+    academic_level          : Literal['Undergraduate', 'Graduate', 'High School']
+    most_used_platform      : Literal['Facebook', 'LinkedIn', 'Instagram', 'Snapchat','Twitter','YouTube', 'TikTok', 'LINE', 'KakaoTalk', 'VKontakte', 'WhatsApp','WeChat']
+    purpose_of_use          : Literal['Networking', 'Education', 'Entertainment', 'News']
+    avg_daily_usage_hours   : float = Field(..., ge=0, le=24)
+    daily_unlocks           : int   = Field(..., ge=0)
+    study_hours             : float = Field(..., ge=0, le=24)
+    physical_activity_hours : float = Field(..., ge=0, le=24)
+    sleep_hours_per_night   : float = Field(..., ge=0, le=24)
+    stress_level            : Literal['Medium', 'Low', 'Very High', 'High']
+
+
+
+
+# Describe what we send back
+class PredictionResponse(BaseModel):
+    predicted_mental_health_score:float
+    #6.777777 -> float
+
+
+
+
+
+
+@app.post('/predict', response_model=PredictionResponse) #6.77777
+def predict(data: StudentData):
+   
+   country_group = data.country if data.country in top_countries else "Other"
+
+   input_row = pd.DataFrame([{
+        'Age'                       :data.age,
+        'Gender'                    :data.gender,
+        'Country'                   :data.country,
+        'Academic_Level'            :data.academic_level,
+        'Most_Used_Platform'        :data.most_used_platform,
+        'Purpose_Of_Use'            :data.purpose_of_use,
+        'Avg_Daily_Usage_Hours'     :data.avg_daily_usage_hours,
+        'Daily_Unlocks'             :data.daily_unlocks,
+        'Study_Hours'               :data.study_hours,
+        'Physical_Activity_Hours'   :data.physical_activity_hours,
+        'Sleep_Hours_Per_Night'     :data.sleep_hours_per_night,
+        'Stress_Level'              :data.stress_level,
+        'Group_country'             :country_group
+   }])
+
+   prediction = model.predict(input_row)[0] #6.77
+   return PredictionResponse(predicted_mental_health_score=round(float(prediction),2))
